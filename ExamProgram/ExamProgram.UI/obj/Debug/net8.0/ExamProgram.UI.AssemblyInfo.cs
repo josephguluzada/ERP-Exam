d0139c279741febd0203a1ad88b718804d89144c
josephguluzada <yquluzade98@gmail.com>
2024-05-27 00:44:26 +0400

    updated error page

--- ExamProgram/ExamProgram.UI/obj/Debug/net8.0/ExamProgram.UI.AssemblyInfo.cs
+++ ExamProgram/ExamProgram.UI/obj/Debug/net8.0/ExamProgram.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExamProgram.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1a8fcec17270ab23bd36b6b03aab5ffdf458cee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98a0688a26db99c477ef57ec55519146292bb637")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExamProgram.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExamProgram.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
